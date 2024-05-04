f1b4e7efe605cdf16f9bc06e2d8f625594376263
jmnis <153198557+jmnis@users.noreply.github.com>
2024-05-04 12:35:08 +0300

    Update scrolling.js

--- scripts/scrolling.js
+++ scripts/scrolling.js
@@ -1,20 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const player = document.getElementById('videoPlayer');
-    const videos = ['video/archive.mp4', 'video/church.mp4', 'video/farm.mp4','video/forest.mp4']; // List your videos here
-    let currentVideo = 1;
+const player = document.getElementById('videoPlayer');
+const videos = ['video/archive.mp4', 'video/church.mp4', 'video/farm.mp4','video/forest.mp4']; // List your videos here
+let currentVideo = 1;
 
-    player.src = videos[currentVideo]; // Ensures the first video is loaded
-    player.play();
+player.src = videos[currentVideo]; // Ensures the first video is loaded
+player.play();
 
-    player.addEventListener('ended', function() {
-        currentVideo++;
-        if (currentVideo < videos.length) {
-            player.src = videos[currentVideo];
-            player.play();
-        } else {
-            currentVideo = 0; // Loop back to the first video
-            player.src = videos[currentVideo];
-            player.play();
-        }
-    });
+player.addEventListener('ended', function() {
+    currentVideo++;
+    if (currentVideo < videos.length) {
+        player.src = videos[currentVideo];
+        player.play();
+    } else {
+        currentVideo = 0; // Loop back to the first video
+        player.src = videos[currentVideo];
+        player.play();
+    }
 });
+
