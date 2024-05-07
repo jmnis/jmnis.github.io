2288c6c89f8b87a1e141c66bb7f02877ddbaf949
jmnis <153198557+jmnis@users.noreply.github.com>
2024-05-07 10:50:23 +0300

    Update scrolling.js

--- scripts/scrolling.js
+++ scripts/scrolling.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('click', function() {
     const player = document.getElementById('videoP');
     const audioPlayer = document.getElementById('audioP');
     const videos = ['video/archive.mp4', 'video/church.mp4', 'video/farm.mp4','video/forest.mp4']; // List your videos here
